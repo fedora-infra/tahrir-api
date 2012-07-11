622412b5a80c0f5759172a9ae51ed743544951ef
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-10 20:55:58 -0400

    version bump for critical bug fix (issue #2)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = [
     ]
 
 setup(name='tahrir-api',
-      version='0.1.3.5',
+      version='0.1.3.6',
       description='An API for interacting with the Tahrir database',
       long_description=README,
       license="AGPLv3+",
