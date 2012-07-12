78fb248966ab7b6cb5154d79037b3be11c802a6f
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-12 14:01:27 -0400

    Modified dbapi to simplify the addition of new people

--- tahrir_api/dbapi.py
+++ tahrir_api/dbapi.py
@@ -131,7 +131,7 @@ class TahrirDatabase(object):
             return person_email
         return False
 
-    def add_person(self, person_id, email):
+    def add_person(self, email):
         """
         Add a new Person to the database
 
@@ -145,7 +145,6 @@ class TahrirDatabase(object):
         session = scoped_session(self.session_maker)
         if not self.person_exists(email):
             new_person = Person(
-                    id=person_id,
                     email=email
                     )
             session.add(new_person)
