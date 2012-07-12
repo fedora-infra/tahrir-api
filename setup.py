d862cd3009b38f8d99fe2042606295fc85303093
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-12 13:08:25 -0400

    Version bump to 0.1.4 for major changes to models

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = [
     ]
 
 setup(name='tahrir-api',
-      version='0.1.3.6',
+      version='0.1.4',
       description='An API for interacting with the Tahrir database',
       long_description=README,
       license="AGPLv3+",
