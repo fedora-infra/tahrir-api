e559c01e015cd1aca5db4ab3c4aaf8dc7c3167db
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-17 18:42:12 -0400

    Version bump for major model changes for model.Assertion: 0.1.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = [
     ]
 
 setup(name='tahrir-api',
-      version='0.1.6',
+      version='0.1.7',
       description='An API for interacting with the Tahrir database',
       long_description=README,
       license="AGPLv3+",
