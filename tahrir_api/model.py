99088b58404b2432a394b7afd49ab6bef1bde6ab
Ralph Bean <rbean@redhat.com>
2013-06-23 22:52:01 -0400

    persons.id is actually an integer, so this foreign key must match.
    sqlite doesn't actually care if they're different types, but postgres
    really complained at me.

--- tahrir_api/model.py
+++ tahrir_api/model.py
@@ -145,7 +145,7 @@ class Invitation(DeclarativeBase):
     created_on = Column(DateTime, nullable=False)
     expires_on = Column(DateTime, nullable=False)
     badge_id = Column(Unicode(128), ForeignKey('badges.id'), nullable=False)
-    created_by = Column(Unicode(128), ForeignKey('persons.id'),
+    created_by = Column(Integer, ForeignKey('persons.id'),
                         nullable=False)
 
     @property
