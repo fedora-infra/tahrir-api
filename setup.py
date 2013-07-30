13a2eec9154a083c7ce5720f9ab2ff2a742f06b2
Ralph Bean <rbean@redhat.com>
2013-07-30 15:34:42 -0400

    0.2.8

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = [
 
 
 setup(name='tahrir-api',
-      version='0.2.7',
+      version='0.2.8',
       description='An API for interacting with the Tahrir database',
       long_description=README,
       license="GPLv3+",
