81cc34ef92478f8c70a6887bd3e5d03923e05f36
Carlos Sanchez <papachoco@gmail.com>
2017-09-29 14:13:27 -0500

    pylint

---
+++ src/tahrir_api/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
