66adf039c645f17caf19b75f996beaf3e3f08ae4
Carlos Sanchez <papachoco@gmail.com>
2017-09-30 23:37:36 -0500

    use hamcrest

--- src/tahrir_api/tests/test_dbapi.py
+++ src/tahrir_api/tests/test_dbapi.py
@@ -1,52 +1,61 @@
-from __future__ import unicode_literals
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from tahrir_api.dbapi import TahrirDatabase
-from tahrir_api.model import DBSession, DeclarativeBase
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import unittest
+
 from sqlalchemy import create_engine
 
+from tahrir_api.dbapi import TahrirDatabase
 
-try:
-    from subprocess import check_output as _check_output
+from tahrir_api.model import DBSession
+from tahrir_api.model import DeclarativeBase
 
-    def check_output(cmd):
-        try:
-            return _check_output(cmd)
-        except:
-            return None
-except:
-    import subprocess
+from subprocess import check_output as _check_output
 
-    def check_output(cmd):
-        try:
-            return subprocess.Popen(
-                cmd, stdout=subprocess.PIPE).communicate()[0]
-        except:
-            return None
+metadata = getattr(DeclarativeBase, 'metadata')
 
 
-class TestDBInit(object):
+def check_output(cmd):
+    try:
+        return _check_output(cmd)
+    except Exception:
+        return None
+
+
+class TestDBInit(unittest.TestCase):
 
     def setUp(self):
         check_output(['touch', 'testdb.db'])
         sqlalchemy_uri = "sqlite:///testdb.db"
         engine = create_engine(sqlalchemy_uri)
         DBSession.configure(bind=engine)
-        DeclarativeBase.metadata.create_all(engine)
+        metadata.create_all(engine)
 
         self.callback_calls = []
         self.api = TahrirDatabase(
             sqlalchemy_uri,
             notification_callback=self.callback)
 
+    def tearDown(self):
+        check_output(['rm', 'testdb.db'])
+        self.callback_calls = []
+
     def callback(self, *args, **kwargs):
         self.callback_calls.append((args, kwargs))
 
     def test_add_badges(self):
         self.api.add_badge(
-            "TestBadge",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
+            u"TestBadge",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
             1337
         )
 
@@ -58,69 +67,69 @@ class TestDBInit(object):
         assert self.api.team_exists("testteam") is True
 
     def test_add_series(self):
-        team_id = self.api.create_team("TestTeam")
+        team_id = self.api.create_team(u"TestTeam")
 
-        self.api.create_series("TestSeries",
-                               "A test series",
+        self.api.create_series(u"TestSeries",
+                               u"A test series",
                                team_id,
-                               "test, series")
+                               u"test, series")
 
         assert self.api.series_exists("testseries") is True
 
     def test_add_milestone(self):
-        team_id = self.api.create_team("TestTeam")
+        team_id = self.api.create_team(u"TestTeam")
 
-        series_id = self.api.create_series("TestSeries",
-                                           "A test series",
+        series_id = self.api.create_series(u"TestSeries",
+                                           u"A test series",
                                            team_id,
-                                           "test, series")
+                                           u"test, series")
 
         badge_id_1 = self.api.add_badge(
-            "TestBadge-1",
-            "TestImage-2",
-            "A test badge for doing 10 unit tests",
-            "TestCriteria",
+            u"TestBadge-1",
+            u"TestImage-2",
+            u"A test badge for doing 10 unit tests",
+            u"TestCriteria",
             1337
         )
 
         badge_id_2 = self.api.add_badge(
-            "TestBadge-2",
-            "TestImage-2",
-            "A test badge for doing 100 unit tests",
-            "TestCriteria",
+            u"TestBadge-2",
+            u"TestImage-2",
+            u"A test badge for doing 100 unit tests",
+            u"TestCriteria",
             1337
         )
 
         milestone_id_1 = self.api.create_milestone(1,
-                                         badge_id_1,
-                                         series_id)
+                                                   badge_id_1,
+                                                   series_id)
 
         milestone_id_2 = self.api.create_milestone(2,
-                                         badge_id_2,
-                                         series_id)
+                                                   badge_id_2,
+                                                   series_id)
 
         assert self.api.milestone_exists(milestone_id_1) is True
         assert self.api.milestone_exists(milestone_id_2) is True
 
     def test_add_person(self):
-        self.api.add_person("test@tester.com")
-        assert self.api.person_exists("test@tester.com") is True
+        self.api.add_person(u"test@tester.com")
+        assert self.api.person_exists(u"test@tester.com") is True
 
     def test_add_issuer(self):
         _id = self.api.add_issuer(
-            "TestOrigin",
-            "TestName",
-            "TestOrg",
-            "TestContact"
+            u"TestOrigin",
+            u"TestName",
+            u"TestOrg",
+            u"TestContact"
         )
         assert self.api.issuer_exists("TestOrigin", "TestName") is True
 
     def test_add_invitation(self):
         badge_id = self.api.add_badge(
-            "TestBadge",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
+            u"TestBadge",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
             1337
         )
         _id = self.api.add_invitation(
@@ -130,7 +139,7 @@ class TestDBInit(object):
         assert self.api.invitation_exists(_id)
 
     def test_last_login(self):
-        email = "test@tester.com"
+        email = u"test@tester.com"
         person_id = self.api.add_person(email)
         person = self.api.get_person(person_id)
         assert not person.last_login
@@ -139,21 +148,21 @@ class TestDBInit(object):
 
     def test_add_assertion(self):
         issuer_id = self.api.add_issuer(
-            "TestOrigin",
-            "TestName",
-            "TestOrg",
-            "TestContact"
+            u"TestOrigin",
+            u"TestName",
+            u"TestOrg",
+            u"TestContact"
         )
         badge_id = self.api.add_badge(
-            "TestBadge",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
+            u"TestBadge",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
             issuer_id,
         )
-        email = "test@tester.com"
-        person_id = self.api.add_person(email)
-        assertion_id = self.api.add_assertion(badge_id, email, None, 'link')
+        email = u"test@tester.com"
+        self.api.add_person(email)
+        self.api.add_assertion(badge_id, email, None, 'link')
         assert self.api.assertion_exists(badge_id, email)
 
         badge = self.api.get_badge(badge_id)
@@ -167,40 +176,40 @@ class TestDBInit(object):
 
     def test_get_badges_from_tags(self):
         issuer_id = self.api.add_issuer(
-            "TestOrigin",
-            "TestName",
-            "TestOrg",
-            "TestContact"
+            u"TestOrigin",
+            u"TestName",
+            u"TestOrg",
+            u"TestContact"
         )
 
         # Badge tagged with "test"
         self.api.add_badge(
-            "TestBadgeA",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
+            u"TestBadgeA",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
             issuer_id,
-            tags="test"
+            tags=u"test"
         )
 
         # Badge tagged with "tester"
         self.api.add_badge(
-            "TestBadgeB",
-            "TestImage",
-            "A second test badge for doing unit tests",
-            "TestCriteria",
+            u"TestBadgeB",
+            u"TestImage",
+            u"A second test badge for doing unit tests",
+            u"TestCriteria",
             issuer_id,
-            tags="tester"
+            tags=u"tester"
         )
 
         # Badge tagged with both "test" and "tester"
         self.api.add_badge(
-            "TestBadgeC",
-            "TestImage",
-            "A third test badge for doing unit tests",
-            "TestCriteria",
+            u"TestBadgeC",
+            u"TestImage",
+            u"A third test badge for doing unit tests",
+            u"TestCriteria",
             issuer_id,
-            tags="test, tester"
+            tags=u"test, tester"
         )
 
         tags = ['test', 'tester']
@@ -208,7 +217,3 @@ class TestDBInit(object):
         assert len(badges_any) == 3
         badges_all = self.api.get_badges_from_tags(tags, match_all=True)
         assert len(badges_all) == 1
-
-    def tearDown(self):
-        check_output(['rm', 'testdb.db'])
-        self.callback_calls = []
--- src/tahrir_api/tests/test_ranking.py
+++ src/tahrir_api/tests/test_ranking.py
@@ -1,95 +1,107 @@
-from __future__ import unicode_literals
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from nose.tools import eq_
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-from tahrir_api.dbapi import TahrirDatabase
-from tahrir_api.model import DBSession, DeclarativeBase, Assertion
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_in
+from hamcrest import has_entry
+from hamcrest import assert_that
+from hamcrest import has_property
+
+import unittest
+import datetime
+
+from subprocess import check_output as _check_output
+
 from sqlalchemy import create_engine
 
+from tahrir_api.dbapi import TahrirDatabase
 
-try:
-    from subprocess import check_output as _check_output
+from tahrir_api.model import Assertion
+from tahrir_api.model import DBSession
+from tahrir_api.model import DeclarativeBase
 
-    def check_output(cmd):
-        try:
-            return _check_output(cmd)
-        except:
-            return None
-except:
-    import subprocess
+metadata = getattr(DeclarativeBase, 'metadata')
 
-    def check_output(cmd):
-        try:
-            return subprocess.Popen(
-                cmd, stdout=subprocess.PIPE).communicate()[0]
-        except:
-            return None
-
-
-import datetime
 now = datetime.datetime.now()
 yesterday = now - datetime.timedelta(days=1)
 one_week_ago = now - datetime.timedelta(days=7)
 one_month_ago = now - datetime.timedelta(weeks=4)
 
 
+def check_output(cmd):
+    try:
+        return _check_output(cmd)
+    except Exception:
+        return None
+
+
 def assert_in(member, container):
-    """ Just like assertTrue(a in b), but with a nicer default message. """
-    if member not in container:
-        raise AssertionError('%r not found in %r' % (member, container))
+    """ 
+    Just like assertTrue(a in b), but with a nicer default message. 
+    """
+    assert_that(member,  is_in(container),
+                '%r not found in %r' % (member, container))
 
 
-class TestRanking(object):
+class TestRanking(unittest.TestCase):
+
+    def _create_test_data(self):
+        issuer_id = self.api.add_issuer(
+            u"TestOrigin",
+            u"TestName",
+            u"TestOrg",
+            u"TestContact"
+        )
+        self.badge_id_1 = self.api.add_badge(
+            u"TestBadge1",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
+            issuer_id,
+        )
+        self.badge_id_2 = self.api.add_badge(
+            u"TestBadge2",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
+            issuer_id,
+        )
+        self.badge_id_3 = self.api.add_badge(
+            u"TestBadge3",
+            u"TestImage",
+            u"A test badge for doing unit tests",
+            u"TestCriteria",
+            issuer_id,
+        )
+        self.email_1 = u"test_1@tester.com"
+        self.api.add_person(self.email_1)
+        self.email_2 = u"test_2@tester.com"
+        self.api.add_person(self.email_2)
+        self.email_3 = u"test_3@tester.com"
+        self.api.add_person(self.email_3)
+        self.email_4 = u"test_4@tester.com"
+        self.api.add_person(self.email_4)
 
     def setUp(self):
         check_output(['touch', 'testdb.db'])
         sqlalchemy_uri = "sqlite:///testdb.db"
         engine = create_engine(sqlalchemy_uri)
         DBSession.configure(bind=engine)
-        DeclarativeBase.metadata.create_all(engine)
+        metadata.create_all(engine)
 
         self.api = TahrirDatabase(sqlalchemy_uri)
         self._create_test_data()
 
     def tearDown(self):
         check_output(['rm', 'testdb.db'])
-
-    def _create_test_data(self):
-        issuer_id = self.api.add_issuer(
-            "TestOrigin",
-            "TestName",
-            "TestOrg",
-            "TestContact"
-        )
-        self.badge_id_1 = self.api.add_badge(
-            "TestBadge1",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
-            issuer_id,
-        )
-        self.badge_id_2 = self.api.add_badge(
-            "TestBadge2",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
-            issuer_id,
-        )
-        self.badge_id_3 = self.api.add_badge(
-            "TestBadge3",
-            "TestImage",
-            "A test badge for doing unit tests",
-            "TestCriteria",
-            issuer_id,
-        )
-        self.email_1 = "test_1@tester.com"
-        person_id_1 = self.api.add_person(self.email_1)
-        self.email_2 = "test_2@tester.com"
-        person_id_2 = self.api.add_person(self.email_2)
-        self.email_3 = "test_3@tester.com"
-        person_id_3 = self.api.add_person(self.email_3)
-        self.email_4 = "test_4@tester.com"
-        person_id_4 = self.api.add_person(self.email_4)
 
     def test_ranking_simple(self):
         self.api.add_assertion(self.badge_id_1, self.email_1, None)
@@ -101,8 +113,8 @@ class TestRanking(object):
         person1 = self.api.get_person("test_1@tester.com")
         person4 = self.api.get_person("test_4@tester.com")
 
-        eq_(person1.rank, 2)
-        eq_(person4.rank, 1)
+        assert_that(person1, has_property('rank', is_(2)))
+        assert_that(person4, has_property('rank', is_(1)))
 
     def test_ranking_tie(self):
         self.api.add_assertion(self.badge_id_1, self.email_1, None)
@@ -122,13 +134,15 @@ class TestRanking(object):
         person3 = self.api.get_person("test_3@tester.com")
         person4 = self.api.get_person("test_4@tester.com")
 
-        eq_(person1.rank, 4)
-        eq_(person2.rank, 2)
-        eq_(person3.rank, 2)
-        eq_(person4.rank, 1)
+        assert_that(person1, has_property('rank', is_(4)))
+        assert_that(person2, has_property('rank', is_(2)))
+        assert_that(person3, has_property('rank', is_(2)))
+        assert_that(person4, has_property('rank', is_(1)))
 
     def test_ranking_preexisting(self):
-        """ Test that rank updating works for pre-existant users """
+        """ 
+        Test that rank updating works for pre-existant users 
+        """
         person1 = self.api.get_person("test_1@tester.com")
         new_assertion1 = Assertion(
             badge_id=self.badge_id_1,
@@ -144,18 +158,18 @@ class TestRanking(object):
 
         # For persons who existed *before* we added cached ranks, they should
         # have a null-rank.
-        eq_(person1.rank, None)
+        assert_that(person1, has_property('rank', is_(none())))
 
         # But once *anyone* else gets a badge, old ranks should be updated too.
         self.api.add_assertion(self.badge_id_1, self.email_2, None)
-        eq_(person1.rank, 1)
+        assert_that(person1.rank, 1)
 
         person2 = self.api.get_person("test_2@tester.com")
-        eq_(person2.rank, 2)
+        assert_that(person2, has_property('rank', is_(2)))
 
         # but people with no badges should still be null ranked.
         person3 = self.api.get_person("test_3@tester.com")
-        eq_(person3.rank, None)
+        assert_that(person3, has_property('rank', is_(none())))
 
     def test_ranking_with_time_limits(self):
         self.api.add_assertion(self.badge_id_1, self.email_1, yesterday)
@@ -170,13 +184,19 @@ class TestRanking(object):
         epsilon = datetime.timedelta(hours=1)
 
         results = self.api._make_leaderboard(yesterday - epsilon, now)
-        eq_(results[person1]['badges'], 1)
-        eq_(results[person4]['badges'], 1)
+        assert_that(results,
+                    has_entry(person1, has_entry('badges', is_(1))))
+        assert_that(results,
+                    has_entry(person4, has_entry('badges', is_(1))))
 
         results = self.api._make_leaderboard(one_week_ago - epsilon, now)
-        eq_(results[person1]['badges'], 1)
-        eq_(results[person4]['badges'], 2)
+        assert_that(results,
+                    has_entry(person1, has_entry('badges', is_(1))))
+        assert_that(results,
+                    has_entry(person4, has_entry('badges', is_(2))))
 
         results = self.api._make_leaderboard(one_month_ago - epsilon, now)
-        eq_(results[person1]['badges'], 1)
-        eq_(results[person4]['badges'], 3)
+        assert_that(results,
+                    has_entry(person1, has_entry('badges', is_(1))))
+        assert_that(results,
+                    has_entry(person4, has_entry('badges', is_(3))))
