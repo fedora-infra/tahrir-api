3c6f113827eaf45e5017c9d9ea58cb11f7444c6a
Carlos Sanchez <papachoco@gmail.com>
2017-10-01 00:53:07 -0500

    use hamcrest

--- src/tahrir_api/tests/test_dbapi.py
+++ src/tahrir_api/tests/test_dbapi.py
@@ -8,6 +8,14 @@ from __future__ import absolute_import
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_in
+from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import has_property
+
 import unittest
 
 from sqlalchemy import create_engine
@@ -41,7 +49,8 @@ class TestDBInit(unittest.TestCase):
         self.callback_calls = []
         self.api = TahrirDatabase(
             sqlalchemy_uri,
-            notification_callback=self.callback)
+            notification_callback=self.callback
+        )
 
     def tearDown(self):
         check_output(['rm', 'testdb.db'])
@@ -52,10 +61,10 @@ class TestDBInit(unittest.TestCase):
 
     def test_add_badges(self):
         self.api.add_badge(
-            u"TestBadge",
-            u"TestImage",
-            u"A test badge for doing unit tests",
-            u"TestCriteria",
+            "TestBadge",
+            "TestImage",
+            "A test badge for doing unit tests",
+            "TestCriteria",
             1337
         )
 
@@ -63,40 +72,39 @@ class TestDBInit(unittest.TestCase):
 
     def test_add_team(self):
         self.api.create_team("TestTeam")
-
-        assert self.api.team_exists("testteam") is True
+        assert_that(self.api.team_exists("testteam"), is_(True))
 
     def test_add_series(self):
-        team_id = self.api.create_team(u"TestTeam")
+        team_id = self.api.create_team("TestTeam")
 
-        self.api.create_series(u"TestSeries",
-                               u"A test series",
+        self.api.create_series("TestSeries",
+                               "A test series",
                                team_id,
-                               u"test, series")
+                               "test, series")
 
-        assert self.api.series_exists("testseries") is True
+        assert_that(self.api.series_exists("testseries"), is_(True))
 
     def test_add_milestone(self):
-        team_id = self.api.create_team(u"TestTeam")
+        team_id = self.api.create_team("TestTeam")
 
-        series_id = self.api.create_series(u"TestSeries",
-                                           u"A test series",
+        series_id = self.api.create_series("TestSeries",
+                                           "A test series",
                                            team_id,
-                                           u"test, series")
+                                           "test, series")
 
         badge_id_1 = self.api.add_badge(
-            u"TestBadge-1",
-            u"TestImage-2",
-            u"A test badge for doing 10 unit tests",
-            u"TestCriteria",
+            "TestBadge-1",
+            "TestImage-2",
+            "A test badge for doing 10 unit tests",
+            "TestCriteria",
             1337
         )
 
         badge_id_2 = self.api.add_badge(
-            u"TestBadge-2",
-            u"TestImage-2",
-            u"A test badge for doing 100 unit tests",
-            u"TestCriteria",
+            "TestBadge-2",
+            "TestImage-2",
+            "A test badge for doing 100 unit tests",
+            "TestCriteria",
             1337
         )
 
@@ -108,112 +116,115 @@ class TestDBInit(unittest.TestCase):
                                                    badge_id_2,
                                                    series_id)
 
-        assert self.api.milestone_exists(milestone_id_1) is True
-        assert self.api.milestone_exists(milestone_id_2) is True
+        assert_that(self.api.milestone_exists(milestone_id_1), is_(True))
+        assert_that(self.api.milestone_exists(milestone_id_2), is_(True))
 
     def test_add_person(self):
-        self.api.add_person(u"test@tester.com")
-        assert self.api.person_exists(u"test@tester.com") is True
+        self.api.add_person("test@tester.com")
+        assert_that(self.api.person_exists("test@tester.com"), is_(True))
 
     def test_add_issuer(self):
         _id = self.api.add_issuer(
-            u"TestOrigin",
-            u"TestName",
-            u"TestOrg",
-            u"TestContact"
+            "TestOrigin",
+            "TestName",
+            "TestOrg",
+            "TestContact"
         )
-        assert self.api.issuer_exists("TestOrigin", "TestName") is True
+        assert_that(self.api.issuer_exists("TestOrigin", "TestName"), is_(True))
 
     def test_add_invitation(self):
         badge_id = self.api.add_badge(
-            u"TestBadge",
-            u"TestImage",
-            u"A test badge for doing unit tests",
-            u"TestCriteria",
+            "TestBadge",
+            "TestImage",
+            "A test badge for doing unit tests",
+            "TestCriteria",
             1337
         )
         _id = self.api.add_invitation(
             badge_id,
         )
 
-        assert self.api.invitation_exists(_id)
+        assert_that(self.api.invitation_exists(_id), is_(True))
 
     def test_last_login(self):
-        email = u"test@tester.com"
+        email = "test@tester.com"
         person_id = self.api.add_person(email)
         person = self.api.get_person(person_id)
-        assert not person.last_login
+        assert_that(person, has_property('last_login', is_(none())))
         self.api.note_login(nickname=person.nickname)
-        assert person.last_login
+        assert_that(person, has_property('last_login', is_not(none())))
 
     def test_add_assertion(self):
         issuer_id = self.api.add_issuer(
-            u"TestOrigin",
-            u"TestName",
-            u"TestOrg",
-            u"TestContact"
+            "TestOrigin",
+            "TestName",
+            "TestOrg",
+            "TestContact"
         )
         badge_id = self.api.add_badge(
-            u"TestBadge",
-            u"TestImage",
-            u"A test badge for doing unit tests",
-            u"TestCriteria",
+            "TestBadge",
+            "TestImage",
+            "A test badge for doing unit tests",
+            "TestCriteria",
             issuer_id,
         )
-        email = u"test@tester.com"
+        email = "test@tester.com"
         self.api.add_person(email)
         self.api.add_assertion(badge_id, email, None, 'link')
-        assert self.api.assertion_exists(badge_id, email)
+        assert_that(self.api.assertion_exists(badge_id, email), is_(True))
 
         badge = self.api.get_badge(badge_id)
-        assert badge.assertions[0].issued_for == 'link'
+        assert_that(badge, 
+                    has_property('assertions', has_length(1)))
+        assert_that(badge.assertions[0],
+                    has_property('issued_for', is_('link')))
 
         # Ensure that we would have published two fedmsg messages for that.
-        assert len(self.callback_calls) == 2
+        assert_that(self.callback_calls, has_length(2))
 
         # Ensure that the first message had a 'badge_id' in the message.
-        assert 'badge_id' in self.callback_calls[0][1]['msg']['badge']
+        assert_that('badge_id', is_in(self.callback_calls[0][1]['msg']['badge']))
 
     def test_get_badges_from_tags(self):
         issuer_id = self.api.add_issuer(
-            u"TestOrigin",
-            u"TestName",
-            u"TestOrg",
-            u"TestContact"
+            "TestOrigin",
+            "TestName",
+            "TestOrg",
+            "TestContact"
         )
 
         # Badge tagged with "test"
         self.api.add_badge(
-            u"TestBadgeA",
-            u"TestImage",
-            u"A test badge for doing unit tests",
-            u"TestCriteria",
+            "TestBadgeA",
+            "TestImage",
+            "A test badge for doing unit tests",
+            "TestCriteria",
             issuer_id,
-            tags=u"test"
+            tags="test"
         )
 
         # Badge tagged with "tester"
         self.api.add_badge(
-            u"TestBadgeB",
-            u"TestImage",
-            u"A second test badge for doing unit tests",
-            u"TestCriteria",
+            "TestBadgeB",
+            "TestImage",
+            "A second test badge for doing unit tests",
+            "TestCriteria",
             issuer_id,
-            tags=u"tester"
+            tags="tester"
         )
 
         # Badge tagged with both "test" and "tester"
         self.api.add_badge(
-            u"TestBadgeC",
-            u"TestImage",
-            u"A third test badge for doing unit tests",
-            u"TestCriteria",
+            "TestBadgeC",
+            "TestImage",
+            "A third test badge for doing unit tests",
+            "TestCriteria",
             issuer_id,
-            tags=u"test, tester"
+            tags="test, tester"
         )
 
         tags = ['test', 'tester']
         badges_any = self.api.get_badges_from_tags(tags, match_all=False)
-        assert len(badges_any) == 3
+        assert_that(badges_any, has_length(3))
         badges_all = self.api.get_badges_from_tags(tags, match_all=True)
-        assert len(badges_all) == 1
+        assert_that(badges_all, has_length(1))
