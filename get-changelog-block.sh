0b7349b379cdaacada87fac234cfe6a7f070f03f
Aurélien Bompard <aurelien@bompard.org>
2024-04-16 16:53:30 +0200

    Add a script to generate the changelog block
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ get-changelog-block.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Generate a block to put in the CHANGELOG.rst file.
+#
+# TODO: convert the changelog to Markdown, so that the Github action can pick it up on release.
+# TODO: replace this by towncrier
+
+set -e
+
+COMMIT_URL=https://github.com/fedora-infra/tahrir-api/commit
+
+last_tag=$(git tag --sort=creatordate | tail -n 1)
+git log ${last_tag}..HEAD --no-merges --reverse --format="- %s (\`%h <${COMMIT_URL}/%h>\`_)"
